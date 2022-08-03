793778542b7380298b6234ba0cfa371a135a9432
Diana Patron <patrondiana1309@gmail.com>
2022-08-02 19:20:55 -0500

    Update config imports

--- load_db.py
+++ load_db.py
@@ -1,9 +1,9 @@
 import boto3
-from config import *
-from sqlalchemy import create_engine
+import config as conf
 import pandas as pd
+from sqlalchemy import create_engine
 
-engine = create_engine(f"mysql://{mysql_db_user}:{mysql_db_pwd}@{mysql_db_host}:{mysql_db_port}/{mysql_db_name}")
+engine = create_engine(f"mysql://{conf.mysql_db_user}:{conf.mysql_db_pwd}@{conf.mysql_db_host}:{conf.mysql_db_port}/{conf.mysql_db_name}")
 conn = engine.connect()
 
 # Reads from the dockerized mysql
@@ -15,9 +15,9 @@ session = boto3.Session(profile_name='default')
 client = session.client('rds')
 
 # Generates the signed IAM authentication token
-token = client.generate_db_auth_token(DBHostname=pg_db_host, Port=pg_db_port, DBUsername=pg_db_user, Region=pg_db_region)
+token = client.generate_db_auth_token(DBHostname=conf.pg_db_host, Port=conf.pg_db_port, DBUsername=conf.pg_db_user, Region=conf.pg_db_region)
 
-pg_engine = create_engine(f"postgresql+psycopg2://{pg_db_user}:{token}@{pg_db_host}:{pg_db_port}/{pg_db_name}")
+pg_engine = create_engine(f"postgresql+psycopg2://{conf.pg_db_user}:{token}@{conf.pg_db_host}:{conf.pg_db_port}/{conf.pg_db_name}")
 conn = pg_engine.connect()
 
 # Writes into AWS RDS instance
